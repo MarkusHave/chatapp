0bd5282df82ea863cf237753cdf2d670fc505c90
Markus Haverinen <markushave@gmail.com>
2020-12-10 11:59:48 +0200

    added messages and config modules

--- chatapp-backend/src/app.module.ts
+++ chatapp-backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MessagesModule, ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

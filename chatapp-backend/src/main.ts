91c157d559c998021f1ac5c57e2f2ffc7aa3834c
Markus Haverinen <markushave@gmail.com>
2020-12-10 12:39:18 +0200

    static assets

--- chatapp-backend/src/main.ts
+++ chatapp-backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './adapters/redis.adapter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const PORT = parseInt(process.env.SERVER_PORT) || 3000;
+
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
+  app.useStaticAssets(join(__dirname, '..', 'res'));
+  await app.listen(PORT);
 }
 bootstrap();

534b09b1ed74a6fa89917be5c81e8e5608609461
Markus Haverinen <markushave@gmail.com>
2020-12-12 13:46:53 +0200

    messages gateway refactoring

--- chatapp-backend/src/messages/messages.gateway.ts
+++ chatapp-backend/src/messages/messages.gateway.ts
@@ -5,45 +5,54 @@ import {
   WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  WsResponse,
+  MessageBody,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
-import { Socket } from 'socket.io';
-import { Server } from 'ws';
+import { Socket, Server } from 'socket.io';
+import { MessageDTO } from './message.dto';
 
 @WebSocketGateway({ namespace: '/chat' })
 export class MessagesGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  // Init WebSocket server
   @WebSocketServer() server: Server;
 
-  private logger: Logger = new Logger('MessageGateway');
+  // Init logger
+  private logger: Logger = new Logger(MessagesGateway.name);
 
   public afterInit(): void {
     return this.logger.log('Init');
   }
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: any): Promise<WsResponse<any>> {
-    return this.server.to(payload.room).emit('msgToClient', payload);
+  handleMessage(@MessageBody() data: MessageDTO): void {
+    this.server.to(data.room).emit('msgToClient', data);
   }
 
   @SubscribeMessage('joinRoom')
-  public joinRoom(client: Socket, room: string): void {
+  public joinRoom(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() room: string,
+  ): void {
     client.join(room);
     client.emit('joinedRoom', room);
   }
 
   @SubscribeMessage('leaveRoom')
-  public leaveRoom(client: Socket, room: string): void {
+  public leaveRoom(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() room: string,
+  ): void {
     client.leave(room);
     client.emit('leftRoom', room);
   }
 
-  public handleDisconnect(client: Socket): void {
+  public handleDisconnect(@ConnectedSocket() client: Socket): void {
     return this.logger.log(`Client: ${client.id} disconnected`);
   }
 
-  public handleConnection(client: Socket): void {
+  public handleConnection(@ConnectedSocket() client: Socket): void {
     return this.logger.log(`Client: ${client.id} connected`);
   }
 }

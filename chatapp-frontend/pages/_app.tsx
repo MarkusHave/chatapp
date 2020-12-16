15465cf214090924da6cb41855205deb40ac6517
Markus Haverinen <markushave@gmail.com>
2020-12-16 16:04:34 +0200

    changed format form .js to .tsx

---
+++ chatapp-frontend/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Chat App</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;

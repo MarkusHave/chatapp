d2fa7d6446d1ce4b527f5256382d68d0d60c60cc
Markus Haverinen <markushave@gmail.com>
2020-12-17 14:48:38 +0200

    login page init

---
+++ chatapp-frontend/pages/login.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+
+const Login = () => {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div>Login</div>
+    </>
+  );
+};
+
+export default Login;

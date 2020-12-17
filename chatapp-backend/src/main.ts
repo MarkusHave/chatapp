1cb393a64b9d3c3150886ebc7e3afe79a111fbe6
Markus Haverinen <markushave@gmail.com>
2020-12-17 14:47:11 +0200

    added cors and removed static files serving

--- chatapp-backend/src/main.ts
+++ chatapp-backend/src/main.ts
@@ -1,15 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './adapters/redis.adapter';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.SERVER_PORT) || 3000;
 
+  app.enableCors({ origin: true });
   app.useWebSocketAdapter(new RedisIoAdapter(app));
-  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
   });

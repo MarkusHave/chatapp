682bd24871cc32f754c5fe87da0249de06efd616
Markus Haverinen <markushave@gmail.com>
2020-12-29 13:47:24 +0200

    add getOne method

--- chatapp-backend/src/rooms/rooms.service.ts
+++ chatapp-backend/src/rooms/rooms.service.ts
@@ -3,28 +3,34 @@ import { RoomsDTO } from './rooms.dto';
 
 @Injectable()
 export class RoomsService {
+  private readonly mockData = [
+    {
+      id: 1,
+      name: 'Room 1',
+    },
+    {
+      id: 2,
+      name: 'Room 2',
+    },
+    {
+      id: 3,
+      name: 'Room 3',
+    },
+    {
+      id: 4,
+      name: 'Room 4',
+    },
+    {
+      id: 5,
+      name: 'Room 5',
+    },
+  ];
+
   async getAll(): Promise<Array<RoomsDTO>> {
-    return [
-      {
-        id: 1,
-        name: 'Room 1',
-      },
-      {
-        id: 2,
-        name: 'Room 2',
-      },
-      {
-        id: 3,
-        name: 'Room 3',
-      },
-      {
-        id: 4,
-        name: 'Room 4',
-      },
-      {
-        id: 5,
-        name: 'Room 5',
-      },
-    ];
+    return this.mockData;
+  }
+
+  async getOne(roomId: number): Promise<RoomsDTO> {
+    return this.mockData.find((room) => room.id === roomId);
   }
 }

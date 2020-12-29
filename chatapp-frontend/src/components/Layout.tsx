ca3d87a034a80c96926bcb75b833745fbda0babb
Markus Haverinen <markushave@gmail.com>
2020-12-29 13:49:32 +0200

    added Layout component

---
+++ chatapp-frontend/src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { Container, Box, Typography } from '@material-ui/core';
+
+const Layout = (props) => (
+  <Container maxWidth='sm'>
+    <Head>
+      <title>Chat App</title>
+    </Head>
+    <Box>
+      <Typography variant='h4'>Chat App</Typography>
+      <Box>{props.children}</Box>
+    </Box>
+  </Container>
+);
+
+export default Layout;

674e517fd9b1aef80ee5c3d95541302b432739a4
Markus Haverinen <markushave@gmail.com>
2021-02-19 20:54:34 +0200

    added layout wrapper

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,18 +2,21 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.css';
 
+import Layout from '../src/components/Layout';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Chat App</title>
+        <meta charSet='utf-8' />
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width'
         />
       </Head>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 

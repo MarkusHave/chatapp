ff6149a563e093c90548607c7d4fdbdb0b62dfac
Markus Haverinen <markushave@gmail.com>
2021-03-03 18:56:04 +0200

    added more TSX

--- frontend/src/components/ChatView/index.tsx
+++ frontend/src/components/ChatView/index.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography, TextField, Button } from '@material-ui/core';
+import styles from '../../../styles/Chat.module.css';
 
 import { Message, Room } from '../../interfaces';
-
+import MessagesList from './MessagesList';
 interface ChatViewProps {
   room: Room;
   messages: Array<Message>;
+  sendMessage: (messageBody: string) => Promise<void>;
 }
 
-const ChatView = ({ room, messages }: ChatViewProps) => {
+const ChatView = ({ room, messages, sendMessage }: ChatViewProps) => {
+  const [messageBody, setMessageBody] = useState<string>();
+
   return (
     <Box>
-      <Typography>{room.name}</Typography>
-      <Box>Chat Here</Box>
-      <TextField label='Message' />
-      <Button variant='contained' color='primary'>
-        Send
-      </Button>
+      <Typography variant='h5'>{room.name}</Typography>
 
-      <Box> </Box>
+      <Box className={styles.chat}>
+        <Box className={styles.messagesList}>
+          <MessagesList messages={messages} />
+        </Box>
+
+        <Box className={styles.messageInputBox}>
+          <TextField
+            size='medium'
+            label='Message'
+            className={styles.messageInput}
+            inputProps={{ maxLength: 100 }}
+            onChange={({ target: { value } }) => setMessageBody(value)}
+          />
+
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={() => sendMessage(messageBody)}>
+            Send
+          </Button>
+        </Box>
+      </Box>
     </Box>
   );
 };

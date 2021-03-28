b2c9dbd7d985fb3bf250fdc2c4e2b2de168abe64
Markus Haverinen <markushave@gmail.com>
2021-03-28 16:21:38 +0300

    added logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './adapters/redis.adapter';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.SERVER_PORT) || 3000;
+  const logger = new Logger('Main');
 
   app.enableCors({ origin: true });
   app.useWebSocketAdapter(new RedisIoAdapter(app));
+
   await app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+    logger.debug(`Server started on port ${PORT}`);
   });
 }
 bootstrap();

da4ef8b584582be5e8df7bc324e0461a32e86398
Markus Haverinen <markushave@gmail.com>
2021-03-28 13:52:21 +0300

    added set username input

--- frontend/src/components/RoomsList/index.tsx
+++ frontend/src/components/RoomsList/index.tsx
@@ -1,12 +1,39 @@
-import { Box, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { Box, Button, TextField, Typography } from '@material-ui/core';
 
 import { Room } from '../../interfaces';
 import RoomListItem from './RoomListItem';
+import { UserContext } from '../../userContext';
 
 const RoomsList = ({ chatRooms }: { chatRooms: Array<Room> }) => {
+  const [username, setUsername] = useState<string>('');
+  const { setUser, user } = useContext(UserContext);
+
+  const handleNameChange = async () => {
+    setUser(username);
+    localStorage.setItem('user', username);
+    setUsername('');
+  };
+
   return (
     <>
+      <Box display='flex' justifyItems='center' alignItems='center'>
+        <TextField
+          size='medium'
+          label='Username'
+          value={username}
+          variant='outlined'
+          inputProps={{ maxLength: 100 }}
+          onChange={({ target: { value } }) => setUsername(value)}
+        />
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => handleNameChange()}>
+          Set username
+        </Button>
+      </Box>
+
       <Typography variant='h6'>Available chat rooms:</Typography>
       <Box>
         {chatRooms.map((room: Room) => {

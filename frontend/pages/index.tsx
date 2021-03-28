eb808f734e77bb3366f5136db7534d0d2cebfa30
Markus Haverinen <markushave@gmail.com>
2021-03-28 13:50:49 +0300

    add usercontext provider

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 import { Room } from '../src/interfaces';
 import { RoomsList } from '../src/components';
+import { UserContext } from '../src/userContext';
 
 export const getStaticProps: GetStaticProps = async () => {
   // Get all chat rooms from backend
@@ -26,10 +27,17 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Home = ({ chatRooms }: { chatRooms: Array<Room> }) => {
+  const [user, setUser] = useState<string>('');
+  const usercontextValue = { user, setUser };
+
   if (!chatRooms) {
     return <Typography>Loading...</Typography>;
   } else {
-    return <RoomsList chatRooms={chatRooms} />;
+    return (
+      <UserContext.Provider value={usercontextValue}>
+        <RoomsList chatRooms={chatRooms} />
+      </UserContext.Provider>
+    );
   }
 };
 
